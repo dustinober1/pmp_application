21d8d01c53a1a4c3dc191b876baa637adfbeb141
dustinober1 <dober.code@gmail.com>
2025-12-21 19:39:24 -0500

    Refactor progress controller to use shared Prisma instance and fix type errors

--- src/controllers/progress.ts
+++ src/controllers/progress.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '../services/database';
 
 /**
  * Get user's complete progress dashboard data
